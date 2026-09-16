bdbeb65a0a677a16b350405174183306b8735f0b
Ocatvio Caballero <Octaandres07@gmail.com>
2026-09-16 10:07:16 -0300

    agregado el archivo dias de la semana

---
+++ src/DiaSemana.java
@@ -0,0 +1,33 @@
+public class DiaSemana {
+    public static void main(String[] args) {
+        System.out.println("***Dia de la semana con Switch***");
+        int Dia = 9; //suponiendo que lunes : 1, martes : 2, estc ...
+        switch (Dia) {
+            case 1:
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Miercoles");
+                break;
+            case 4:
+                System.out.println("Jueves");
+                break;
+            case 5:
+                System.out.println("Viernes");
+                break;
+            case 6:
+                System.out.println("Sabado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default:
+                System.out.println("Dia invalido");
+                break;
+
+        }
+    }
+}
